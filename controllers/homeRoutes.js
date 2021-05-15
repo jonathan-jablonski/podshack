10ff2d0d360defd3c6e9814f961f6ba833537f6f
Cigi Tipton <cigi@mirrorlessmind.com>
2021-05-15 18:11:09 -0500

    changes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
-const apiRoutes = require("./api");
+//const apiRoutes = require("./controllers");
 const homeRoutes = require("./homeRoutes");
 
 router.use("/", homeRoutes);
-router.use("/api", apiRoutes);
+//router.use("/", apiRoutes);
 
 module.exports = router;
