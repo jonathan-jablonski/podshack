41f856638d5b689024d29644b84557bec23eeb64
Cigi <cigi@mirrorlessmind.com>
2021-05-17 21:36:18 -0500

    updated from class

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,5 @@
 const router = require('express').Router();
 
-//const apiRoutes = require("./controllers");
-const homeRoutes = require("./homeRoutes");
-
-router.use("/", homeRoutes);
-//router.use("/", apiRoutes);
 router.get('/login', (req, res) => { 
   res.render('login');
 });
