93ce959ad169972d8dece14d3b498a1ac8882f07
Cigi <cigi@mirrorlessmind.com>
2021-05-17 21:37:30 -0500

    updated from class

--- controllers/index.js
+++ controllers/index.js
@@ -1,22 +1,11 @@
 const router = require("express").Router();
-
+const apiRoutes = require('./api');
 const homeRoutes = require("./homeRoutes");
 const authRoutes = require("../routes/auth-routes.js");
-// const apiRoutes = require('./api');
+
 
 router.use('/', homeRoutes);
 router.use ('/auth', authRoutes);
-// router.use('/api', apiRoutes);
-
-
-module.exports = router;
-=======
-const router = require("express").Router();
-
-const homeRoutes = require("./homeRoutes");
-const apiRoutes = require('./api');
-
-router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 
