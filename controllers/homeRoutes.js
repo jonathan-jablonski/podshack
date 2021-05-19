b84ecedf8a80df922c3c694d151ceea096ab8bc1
jonathan-jablonski <jonathanjablonski94@gmail.com>
2021-05-19 17:26:52 -0500

    Code runs with no errors, but creating account is not working, search bar not showing up

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,51 +1,52 @@
-const router = require('express').Router();
-const { Client } = require('podcast-api');
+const router = require("express").Router();
+const { Client } = require("podcast-api");
 // const withAuth = require('../utils/auth')
 
-
 const client = Client({
-  apiKey: process.env.API_KEY || null
+  apiKey: process.env.API_KEY || null,
 });
 
-router.get('/', (req, res) => {
-res.render('homepage')
-  
-router.get('/login', (req, res) => { 
-  res.render('login');
+router.get("/", (req, res) => {
+  res.render("homepage");
 });
 
-router.get('/search', (req, res) => {
-  res.render('search',
-    // { 
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/search", (req, res) => {
+  res.render(
+    "search"
+    // {
     //   logged_in: req.session.logged_in
     //  }
   );
 });
 
-router.get('/results', (req, res) => {
+router.get("/results", (req, res) => {
   try {
-    client.search({
-      q: 'star wars',
-      type: 'episode',
-      offset: 0,
-      only_in: 'title,description,author',
-      language: 'English',
-      safe_mode: 0,
-    })
+    client
+      .search({
+        q: "star wars",
+        type: "episode",
+        offset: 0,
+        only_in: "title,description,author",
+        language: "English",
+        safe_mode: 0,
+      })
       .then((response) => {
-        const podcasts = response.data.results.map(podcast => {
+        const podcasts = response.data.results.map((podcast) => {
           return {
             title: podcast.title,
-            description: podcast.description
+            description: podcast.description,
           };
         });
-        res.render('results', {
+        res.render("results", {
           podcasts,
           // loggedIn: req.session.loggedIn
         });
       });
-  }
-  catch (err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
