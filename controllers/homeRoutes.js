39b9d197e53cc0cae1e3af2a7328b95795df387d
Jonathan J <jonathanjablonski94@gmail.com>
2021-05-20 14:51:21 -0500

    updated this branch with updated files from 05/19/21

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,16 +1,17 @@
 const router = require("express").Router();
 const { Client } = require("podcast-api");
+const Podcast = require("../model/podcast");
 // const withAuth = require('../utils/auth')
 
 const client = Client({
-  apiKey: process.env.API_KEY || null,
+  apiKey: process.env.LISTEN_API_KEY || null,
 });
 
 router.get("/", (req, res) => {
-  res.render("homepage");
-});
-
-router.get("/login", (req, res) => {
+  // if (req.session.logged_in) {
+  //   res.redirect('/search');
+  //   return;
+  // }
   res.render("login");
 });
 
@@ -23,11 +24,34 @@ router.get("/search", (req, res) => {
   );
 });
 
-router.get("/results", (req, res) => {
+router.get("/results/random", (req, res) => {
+  try {
+    client.justListen({}).then((response) => {
+      console.log("this is data:", response.data);
+      const podcasts = [
+        {
+          link: response?.data?.link,
+          thumbnail: response?.data?.thumbnail,
+          title: response?.data?.title,
+          description: response?.data?.description,
+        },
+      ];
+      console.log(podcasts);
+      res.render("results", {
+        podcasts,
+        // loggedIn: req.session.loggedIn
+      });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/results/:userinput", (req, res) => {
   try {
     client
       .search({
-        q: "star wars",
+        q: req.params.userinput,
         type: "episode",
         offset: 0,
         only_in: "title,description,author",
@@ -37,15 +61,51 @@ router.get("/results", (req, res) => {
       .then((response) => {
         const podcasts = response.data.results.map((podcast) => {
           return {
-            title: podcast.title,
-            description: podcast.description,
+            link: podcast.link,
+            thumbnail: podcast.thumbnail,
+            title: podcast.title_original,
+            description: podcast.description_original,
           };
         });
+        // console.log(podcasts)
+        console.log(response.data);
         res.render("results", {
           podcasts,
-          // loggedIn: req.session.loggedIn
+          // logged_in: req.session.logged_in
         });
       });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/profile/playlist", (req, res) => {
+  try {
+    client.fetchMyPlaylists({}).then((response) => {
+      console.log(response.data);
+      res.render("profile", {
+        // loggedIn: req.session.loggedIn
+      });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/profile", async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.name, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Podcast }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
